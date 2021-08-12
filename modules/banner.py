c6f883132fd24da756541f918a853b3536684917
Leon $ <80839783+Snapdays@users.noreply.github.com>
2021-08-12 13:53:26 +0200

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -24,12 +24,28 @@ def banner():
     # clear term for linux / windows
     system('cls;clear')
 
-    print(f"""
-            {purple}┌─────────────────────────────────────────────┐
-            {purple}│                {white}Rx leaker                    {purple}│   {gray}- {black}github.com/{black}BlackRabbit-0{white}
-            {purple}│                 {blue}────────{white}                  {purple}  │
-            {purple}│           {gray}► {white}Version {yellow}{version}{white}         {purple}            │
-            {purple}│           {gray}► {white}Backend {yellow}leaker{white}       {purple}           │
-            {purple}└─────────────────────────────────────────────┘\n
+    print(f"""{black}Hello {green}Admin {black}| {purple}Discord: {white}Leon $#9346 {black}| {cyan}Instagram: {white}@xBackend {black}| {red}Telegram: {white}@xBackend  {black}| {red}New {yellow}Fresh {gray}V1 {cyan}Update {purple}Out!
+
+                            {yellow}            ______              _                     _ 
+                            {yellow}           (____  \            | |                   | |
+                            {yellow}      _   _ ____)  )_____  ____| |  _ _____ ____   __| |
+                            {yellow}     ( \ / )  __  ((____ |/ ___) |_/ ) ___ |  _ \ / _  |
+                            {yellow}      ) X (| |__)  ) ___ ( (___|  _ (| ____| | | ( (_| |
+                            {yellow}     (_/ \_)______/\_____|\____)_| \_)_____)_| |_|\____|
+                                                                                          
+
+                                         {white}╔════════════════{yellow}═══════════════════
+                                         {white}║                                   
+                                         {white}║    {yellow}User {gray}► {red}Admin                               
+                                         {white}║    {yellow}Plan {gray}► {red}Owner                               
+                                         {white}║                                   
+                                         {white}╚════════════════{yellow}═══════════════════
+
+                                         {white}╔════════════════{yellow}═══════════════════
+                                         {white}║                                   
+                                         {white}║    {yellow}Expiry {gray}► {red}106751.99                               
+                                         {white}║    {cyan}VIP {gray}► {green}True    
+                                         {white}║                                                              
+                                         {white}╚════════════════{yellow}═══════════════════                                  \n
                 \n
     """)
